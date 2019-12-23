c1d1161b5928fd2fbb83a5c0d637a89978570bdc
Walid <walid.bizid@esprit.tn>
2019-12-23 19:02:07 +0100

    Initial commit using IntelliJ IDEA

---
+++ src/main/java/com/projet_consolidation/projet_consolidation/ProjetConsolidationApplication.java
@@ -0,0 +1,13 @@
+package com.projet_consolidation.projet_consolidation;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class ProjetConsolidationApplication {
+
+	public static void main(String[] args) {
+		SpringApplication.run(ProjetConsolidationApplication.class, args);
+	}
+
+}
